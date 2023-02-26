58a318c3d1d24df4f341d61afbc2e16b8f261dc0
이태언 <mktears0609@gmail.com>
2023-02-26 15:30:15 +0900

    make card

--- lib/main.dart
+++ lib/main.dart
@@ -23,95 +23,139 @@ class App extends StatelessWidget {
     // * rule - screen must need to have scaffold (provied structure)
     return MaterialApp(
       home: Scaffold(
-          backgroundColor: const Color.fromRGBO(27, 26, 26, 1),
-          body: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 40),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const SizedBox(
-                  height: 80,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.end,
-                      children: [
-                        const Text("Hey, Taeeon",
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 32,
-                                fontWeight: FontWeight.bold)),
-                        Text("welcome, back",
-                            style: TextStyle(
-                              color: Colors.white.withOpacity(0.6),
-                              fontSize: 18,
-                            )),
-                      ],
-                    )
-                  ],
-                ),
-                const SizedBox(
-                  height: 100,
-                ),
-                Text("Total Balance",
+        backgroundColor: const Color.fromRGBO(27, 26, 26, 1),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 40),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(
+                height: 80,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      const Text("Hey, Taeeon",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 32,
+                              fontWeight: FontWeight.bold)),
+                      Text("welcome, back",
+                          style: TextStyle(
+                            color: Colors.white.withOpacity(0.6),
+                            fontSize: 18,
+                          )),
+                    ],
+                  )
+                ],
+              ),
+              const SizedBox(
+                height: 100,
+              ),
+              Text("Total Balance",
+                  style: TextStyle(
+                    fontSize: 22,
+                    color: Colors.white.withOpacity(0.8),
+                  )),
+              const SizedBox(
+                height: 10,
+              ),
+              const Text("\$5 194 482",
+                  style: TextStyle(
+                    fontSize: 44,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.white,
+                  )),
+              const SizedBox(
+                height: 24,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: const [
+                  Button(
+                    text: "Transfer",
+                    bgColor: Colors.amber,
+                    textColor: Colors.black,
+                  ),
+                  Button(
+                    text: "Request",
+                    bgColor: Color.fromARGB(115, 84, 75, 75),
+                    textColor: Colors.white,
+                  )
+                ],
+              ),
+              const SizedBox(
+                height: 100,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  const Text(
+                    "Wallets",
                     style: TextStyle(
-                      fontSize: 22,
-                      color: Colors.white.withOpacity(0.8),
-                    )),
-                const SizedBox(
-                  height: 10,
-                ),
-                const Text("\$5 194 482",
-                    style: TextStyle(
-                      fontSize: 44,
-                      fontWeight: FontWeight.w600,
                       color: Colors.white,
-                    )),
-                const SizedBox(
-                  height: 24,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: const [
-                    Button(
-                      text: "Transfer",
-                      bgColor: Colors.amber,
-                      textColor: Colors.black,
+                      fontSize: 36,
+                      fontWeight: FontWeight.w600,
                     ),
-                    Button(
-                      text: "Request",
-                      bgColor: Color.fromARGB(115, 84, 75, 75),
-                      textColor: Colors.white,
-                    )
-                  ],
-                ),
-                const SizedBox(
-                  height: 100,
-                ),
-                Row(
-                  children: [
-                    const Text(
-                      "Wallets",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 36,
-                        fontWeight: FontWeight.w600,
-                      ),
+                  ),
+                  Text(
+                    "View All",
+                    style: TextStyle(
+                      // * opacity 같은 값은 compile 할때 알기 힘든 값이라 constant가 될 수 없다.
+                      color: Colors.white.withOpacity(0.8),
+                      fontSize: 18,
                     ),
-                    Text(
-                      "View All",
-                      style: TextStyle(
-                        // * opacity 같은 값은 compile 할때 알기 힘든 값이라 constant가 될 수 없다.
-                        color: Colors.white.withOpacity(0.8),
-                      ),
-                    ),
-                  ],
-                )
-              ],
-            ),
-          )),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 40,
+              ),
+              Container(
+                  decoration: const BoxDecoration(
+                    color: Color.fromARGB(115, 84, 75, 75),
+                  ),
+                  child: Row(
+                    children: [
+                      Column(
+                        children: [
+                          const Text(
+                            "Euro",
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          Row(
+                            children: const [
+                              Text(
+                                "6 428",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                ),
+                              ),
+                              Text(
+                                "Euro",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ],
+                          )
+                        ],
+                      )
+                    ],
+                  ))
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
