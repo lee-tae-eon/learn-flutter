90df7ab451269988db7219ca9797116e63e46157
이태언 <mktears0609@gmail.com>
2023-03-01 04:22:51 +0900

    aa

--- lib/main.dart
+++ lib/main.dart
@@ -165,8 +165,14 @@ class App extends StatelessWidget {
                             )
                           ],
                         ),
-                        const Icon(Icons.euro_rounded,
-                            color: Colors.white, size: 98)
+                        Transform.scale(
+                          scale: 2.2,
+                          child: const Icon(
+                            Icons.euro_rounded,
+                            color: Colors.white,
+                            size: 88,
+                          ),
+                        )
                       ],
                     ),
                   ))
