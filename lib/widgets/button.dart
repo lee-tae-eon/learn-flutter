b657cb854548ec2440c0657554349470450227f5
이태언 <mktears0609@gmail.com>
2023-03-03 21:27:49 +0900

    btn padding

--- lib/widgets/button.dart
+++ lib/widgets/button.dart
@@ -22,10 +22,10 @@ class Button extends StatelessWidget {
       // * class의  property들을 받아서 바뀌는 변수로 처리 하는 이상 const가 될 수 없기때문에
       // * const 처리가 되는 edge부분만 const로 변경하면 된다.
       child: Padding(
-          padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 50),
+          padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 60),
           child: Text(text,
               style: TextStyle(
-                fontSize: 16,
+                fontSize: 18,
                 color: textColor,
               ))),
     );
