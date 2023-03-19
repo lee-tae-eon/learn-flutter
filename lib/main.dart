6c69eced2086c4ca872515d99e7ce51a0dc93a7c
이태언 <mktears0609@gmail.com>
2023-03-19 21:51:04 +0900

    initial

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,21 @@
 import 'package:flutter/material.dart';
 
-// * flutter 는 위젯을 조합해서 애플리케이션을 만든다
-// * https://docs.flutter.dev/development/ui/widgets - 공식 widgets
-// * widgets을 만드는 것은 just add class
 void main() {
   runApp(const App());
 }
 
-// ! flutter sdk 에 있는 3개의 core widget중 하나를 extend 해야 한다.
-// ! build method 를 구현해야함.
-// ! flutter가 실행 및 build 메소드가 어떤 것을 return하던지 화면에 구현된다.
-// * constant 는 수정할 수 없고 compile 전에 그 value를 알 수 있는 변수
-
-// * statefull widget  ->  state에 따 유동적으로 바뀌는 ui
-// * 2개의 part로 나뉨 statefulwidget class와 state class
-class App extends StatefulWidget {
+class App extends StatelessWidget {
   const App({super.key});
 
   @override
-  State<App> createState() => _AppState();
-}
-
-class _AppState extends State<App> {
-  // int counter = 0;
-  // List<int> numbers = [];
-  bool showTitle = true;
-
-  void toggleTitle() {
-    setState(() {
-      showTitle = !showTitle;
-    });
-  }
-
-  void onClick() {
-    // * setState 함수 -> State class 에 data가 변경 되었다고 알려주는 함수.
-    // * 실행되어야 하는 코드가 setState에 있지 않아도 실행이 된다.
-    // * setState로 인해 ui가 다시 빌드 되기 때문이다.
-    // counter = counter + 1;
-    // setState(() => {});
-    // print(numbers);
-    // setState(() => {counter = counter + 1, numbers.add(numbers.length)});
-  }
-
-  // * override 부모 클래스의 메소드를 덮어씌움
-  @override
   Widget build(BuildContext context) {
-    // * base가 되는 app  - 구글의 material or ios 의 cupertino중에 선태 -> material 추천
-    // * rule - screen must need to have scaffold (provied structure)
     return MaterialApp(
       theme: ThemeData(
-        textTheme: const TextTheme(
-          titleLarge: TextStyle(
-            color: Colors.red,
-          ),
+          colorScheme: ColorScheme.fromSwatch(
+        backgroundColor: const Color(
+          0xFFE7626C,
         ),
-      ),
-      home: Scaffold(
-        backgroundColor: const Color(0xFFF4EDDB),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              showTitle ? const MyLargeTitle() : const Text("nothing"),
-              IconButton(
-                  onPressed: toggleTitle,
-                  icon: const Icon(Icons.remove_red_eye))
-              // Text(
-              //   "$counter",
-              //   style: const TextStyle(
-              //     fontSize: 20,
-              //   ),
-              // ),
-              // for (var n in numbers) Text('$n'),
-              // IconButton(
-              //     onPressed: onClick,
-              //     icon: const Icon(
-              //       Icons.add_box_rounded,
-              //     ))
-            ],
-          ),
-        ),
-      ),
+      )),
     );
-  }
-}
-
-// state widget => statefull widget으로 변경
-// statefullwidget have lifecycle
-class MyLargeTitle extends StatefulWidget {
-  const MyLargeTitle({
-    super.key,
-  });
-
-  @override
-  State<MyLargeTitle> createState() => _MyLargeTitleState();
-}
-
-class _MyLargeTitleState extends State<MyLargeTitle> {
-  //  부모 요소에 의해 초기화가 되어야 하는경우 사용하는 함수 -> initState
-  // init state methods 는 build method보다 먼저 호출 되어야 한다.
-  // component did mount와 같은 역할?
-  @override
-  void initState() {
-    super.initState();
-    print("initState");
-  }
-
-  // component did unmount 와 같은 역할?
-  // 위젯이 언트리 되기전에 이벤트를 해제 하는 역할.
-  @override
-  void dispose() {
-    super.dispose();
-    print("test");
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    print("buildState");
-    return Text("My Large Title",
-        style: TextStyle(
-            fontSize: 30,
-            color: Theme.of(context).textTheme.titleLarge?.color));
   }
 }
