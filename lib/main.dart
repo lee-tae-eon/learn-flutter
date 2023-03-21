317716ff4ccc896acd19a04208408d7882d96b6f
이태언 <mktears0609@gmail.com>
2023-03-21 22:01:46 +0900

    ADD: color

--- lib/main.dart
+++ lib/main.dart
@@ -14,11 +14,22 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-          colorScheme: ColorScheme.fromSwatch(
-        backgroundColor: const Color(
-          0xFFE7626C,
+        colorScheme: ColorScheme.fromSwatch(
+          backgroundColor: const Color(
+            0xFFE7626C,
+          ),
         ),
-      )),
+        textTheme: const TextTheme(
+          displayLarge: TextStyle(
+            color: Color(
+              0xFF232B55,
+            ),
+          ),
+        ),
+        cardColor: const Color(
+          0xFFF4EDDB,
+        ),
+      ),
     );
   }
 }
