c2ea49ca354d2cc01c4cf60e7f2f144cd87d8c33
LeeTaeEon <mktears0609@gmail.com>
2023-04-19 19:36:55 +0900

    Appbar

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -7,9 +7,17 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     // scaffold는 기본적인 레이아웃과 설정을 제공해줌
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
+        elevation: 3,
+        backgroundColor: const Color.fromARGB(255, 255, 255, 255),
+        foregroundColor: Colors.green,
         title: const Text(
           "Today's Toon's",
+          style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.w600,
+          ),
         ),
       ),
     );
