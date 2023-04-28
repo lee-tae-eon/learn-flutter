e24cb744c8a2aa0ba22ddd82c2c8f72c112a69ad
LeeTaeEon <mktears0609@gmail.com>
2023-04-28 21:37:35 +0900

    convert stateless

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,34 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:toolflix/models/webtoon_model.dart';
-import 'package:toolflix/services/api_service.dart';
 
-class HomeScreen extends StatefulWidget {
+class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  List<WebtoonModel> webtoons = [];
-  bool isLoading = true;
-
-  void waitForWebToons() async {
-    webtoons = await ApiService.getTodaysToons();
-    isLoading = false;
-    setState(() {});
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    waitForWebToons();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    print(isLoading);
-    print(webtoons);
     // scaffold는 기본적인 레이아웃과 설정을 제공해줌
     return Scaffold(
       backgroundColor: Colors.white,
