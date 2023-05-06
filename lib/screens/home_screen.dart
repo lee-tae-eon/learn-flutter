6d5c0e097182ef7164f5457bebe6c14c7e58e00c
LeeTaeEon <mktears0609@gmail.com>
2023-05-06 19:02:23 +0900

    Card decoration

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -62,14 +62,37 @@ class HomeScreen extends StatelessWidget {
         var webtoon = snapshot.data![index];
         return Column(
           children: [
-            Image.network(
-              webtoon.thumb,
-              headers: const {
-                "User-Agent":
-                    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
-              },
+            Container(
+              width: 250,
+              // 자식의 부모 영역 침범을 제하는 방법.
+              clipBehavior: Clip.hardEdge,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(
+                    15,
+                  ),
+                  boxShadow: [
+                    BoxShadow(
+                        blurRadius: 5,
+                        offset: const Offset(0, 0),
+                        color: Colors.black.withOpacity(0.5))
+                  ]),
+              child: Image.network(
+                webtoon.thumb,
+                headers: const {
+                  "User-Agent":
+                      "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
+                },
+              ),
             ),
-            Text(webtoon.title),
+            const SizedBox(
+              height: 10,
+            ),
+            Text(
+              webtoon.title,
+              style: const TextStyle(
+                fontSize: 22,
+              ),
+            ),
           ],
         );
       },
