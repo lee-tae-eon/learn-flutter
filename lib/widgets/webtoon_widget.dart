fba8e33fdaab26212d38d22c2384832dc995c111
LeeTaeEon <mktears0609@gmail.com>
2023-05-07 22:19:43 +0900

    full screen dialog

--- lib/widgets/webtoon_widget.dart
+++ lib/widgets/webtoon_widget.dart
@@ -26,36 +26,38 @@ class Webtoon extends StatelessWidget {
               thumb: thumb,
               id: id,
             ),
+            fullscreenDialog: true,
           ),
         );
       },
       child: Column(
         children: [
           Container(
-              width: 250,
-              // 자식의 부모 영역 침범을 제하는 방법.
-              clipBehavior: Clip.hardEdge,
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(
-                    15,
-                  ),
-                  boxShadow: [
-                    BoxShadow(
-                        blurRadius: 10,
-                        offset: const Offset(10, 10),
-                        color: Colors.black.withOpacity(0.7))
-                  ]),
-              child: AnimatedContainer(
-                duration: const Duration(milliseconds: 300),
-                child: Image.network(
-                  thumb,
-                  headers: const {
-                    "User-Agent":
-                        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
-                    "Referer": "https://comic.naver.com"
-                  },
+            width: 250,
+            // 자식의 부모 영역 침범을 제하는 방법.
+            clipBehavior: Clip.hardEdge,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(
+                  15,
                 ),
-              )),
+                boxShadow: [
+                  BoxShadow(
+                      blurRadius: 10,
+                      offset: const Offset(10, 10),
+                      color: Colors.black.withOpacity(0.7))
+                ]),
+            child: AnimatedContainer(
+              duration: const Duration(milliseconds: 300),
+              child: Image.network(
+                thumb,
+                headers: const {
+                  "User-Agent":
+                      "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
+                  "Referer": "https://comic.naver.com"
+                },
+              ),
+            ),
+          ),
           const SizedBox(
             height: 10,
           ),
