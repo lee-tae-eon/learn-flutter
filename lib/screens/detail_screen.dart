ca7f36319ba15df840006f96fbcaeac4320676f6
LeeTaeEon <mktears0609@gmail.com>
2023-06-12 22:26:03 +0900

    FIX: padding 위치 변경

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -46,57 +46,55 @@ class _DetailScreenState extends State<DetailScreen> {
           ),
         ),
         body: SingleChildScrollView(
-          child: Column(
-            children: [
-              const SizedBox(
-                height: 20,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Hero(
-                    tag: widget.id,
-                    child: Container(
-                      width: 250,
-                      // 자식의 부모 영역 침범을 제하는 방법.
-                      clipBehavior: Clip.hardEdge,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(
-                            15,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 50),
+            child: Column(
+              children: [
+                const SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Hero(
+                      tag: widget.id,
+                      child: Container(
+                        width: 250,
+                        // 자식의 부모 영역 침범을 제하는 방법.
+                        clipBehavior: Clip.hardEdge,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(
+                              15,
+                            ),
+                            boxShadow: [
+                              BoxShadow(
+                                  blurRadius: 10,
+                                  offset: const Offset(10, 10),
+                                  color: Colors.black.withOpacity(0.7))
+                            ]),
+                        child: AnimatedContainer(
+                          duration: const Duration(milliseconds: 300),
+                          child: Image.network(
+                            widget.thumb,
+                            headers: const {
+                              "User-Agent":
+                                  "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
+                              "Referer": "https://comic.naver.com"
+                            },
                           ),
-                          boxShadow: [
-                            BoxShadow(
-                                blurRadius: 10,
-                                offset: const Offset(10, 10),
-                                color: Colors.black.withOpacity(0.7))
-                          ]),
-                      child: AnimatedContainer(
-                        duration: const Duration(milliseconds: 300),
-                        child: Image.network(
-                          widget.thumb,
-                          headers: const {
-                            "User-Agent":
-                                "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
-                            "Referer": "https://comic.naver.com"
-                          },
                         ),
                       ),
                     ),
-                  ),
-                ],
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              FutureBuilder(
-                future: webtoon,
-                builder: (context, snapshot) {
-                  if (snapshot.hasData) {
-                    return Padding(
-                      padding: const EdgeInsets.symmetric(
-                        horizontal: 50,
-                      ),
-                      child: Column(
+                  ],
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                FutureBuilder(
+                  future: webtoon,
+                  builder: (context, snapshot) {
+                    if (snapshot.hasData) {
+                      return Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
                           Text(
@@ -115,29 +113,40 @@ class _DetailScreenState extends State<DetailScreen> {
                             ),
                           ),
                         ],
-                      ),
-                    );
-                  }
-                  return const Text("....");
-                },
-              ),
-              const SizedBox(
-                height: 50,
-              ),
-              FutureBuilder(
-                  future: episodes,
-                  builder: (cotenxt, snapshot) {
-                    if (snapshot.hasData) {
-                      return Column(
-                        children: [
-                          for (var episode in snapshot.data!)
-                            Text(episode.title)
-                        ],
                       );
                     }
-                    return Container();
-                  })
-            ],
+                    return const Text("....");
+                  },
+                ),
+                const SizedBox(
+                  height: 50,
+                ),
+                FutureBuilder(
+                    future: episodes,
+                    builder: (cotenxt, snapshot) {
+                      if (snapshot.hasData) {
+                        return Column(
+                          children: [
+                            for (var episode in snapshot.data!)
+                              Container(
+                                child: Row(
+                                  children: [
+                                    Text(
+                                      episode.title,
+                                    ),
+                                    const Icon(
+                                      Icons.chevron_right_rounded,
+                                    )
+                                  ],
+                                ),
+                              )
+                          ],
+                        );
+                      }
+                      return Container();
+                    })
+              ],
+            ),
           ),
         ));
   }
